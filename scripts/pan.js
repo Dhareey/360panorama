0872fe013024d1ff7b898f066fd6ced4f5fc65e1
Dhareey <damilareadet@gmail.com>
2024-04-23 17:25:45 +0100

    "Removed some of the pictures"

--- scripts/pan.js
+++ scripts/pan.js
@@ -1,7 +1,43 @@
-pannellum.viewer('panorama', {
-    "type": "equirectangular",
-    "panorama": "../panos/outside_entrance_hdr.jpg",
-    "autoLoad": true,
-    "title": "Entrance staircase",
-    "hfov": 300
+viewer = pannellum.viewer('panorama', {
+
+    "default": {
+        "firstScene": "entranceStairs",
+        'author': "OEA Consults Ltd",
+        "sceneFadeDuration": 1000,
+        "autoLoad": true,
+    },
+    "scenes": {
+        "entranceStairs": {
+            "title": "Entrance staircase",
+            "hfov": 300,
+            "type": "equirectangular",
+            "panorama": "scripts/outside_entrance_hdr.jpg",
+            "showControls": false,
+            "hotSpots": [
+                {
+                    "pitch": 24,
+                    "yaw": -1,
+                    "type": "scene",
+                    "text": "Entrance Landing",
+                    "sceneId": "entranceDoor"
+                }
+            ]
+        },
+
+        "entranceDoor": {
+            'title': 'Entrance Landing',
+            "hfov": 300,
+            'type': "equirectangular",
+            "panorama": 'scripts/entrance_door_hdr.jpg',
+            "hotSpots": [
+                {
+                    "pitch": -2.1,
+                    "yaw": 132.9,
+                    "type": "scene",
+                    "text": "Entrance Landing",
+                    "sceneId": "entranceStairs"
+                }
+            ]
+        }
+    }
 });
